79d4bd6ea996976e341de9d1f1fe66f87b61dcc0
Yuelin Du <yuelindu@Yuelins-MacBook-Air.local>
2017-09-23 17:25:40 -0700

    create template

---
+++ talkmeup-cf-template.py
@@ -0,0 +1,75 @@
+from troposphere import (
+    Base64,
+    ec2,
+    GetAtt,
+    Join,
+    Output,
+    Parameter,
+    Ref,
+    Template,
+)
+
+ApplicationPort = "3000"
+
+t = Template()
+t.add_description("Talk Me Up: We empower your words!")
+t.add_parameter(Parameter(
+    "KeyPair",
+    Description="EC2 KeyPair to SSH",
+    Type="AWS::EC2::KeyPair::KeyName",
+    ConstraintDescription="must be the name of an existing EC2 KeyPair.",
+))
+t.add_resource(ec2.SecurityGroup(
+    "SecurityGroup",
+    GroupDescription="Allow SSH and TCP/{} access".format(ApplicationPort),
+    SecurityGroupIngress=[
+        ec2.SecurityGroupRule(
+            IpProtocol="tcp",
+            FromPort="22",
+            ToPort="22",
+            CidrIp="0.0.0.0/0",
+        ),
+        ec2.SecurityGroupRule(
+            IpProtocol="tcp",
+            FromPort=ApplicationPort,
+            ToPort=ApplicationPort,
+            CidrIp="0.0.0.0/0",
+        ),
+    ],
+))
+
+ud = Base64(Join('\n', [
+    "#!/bin/bash",
+    "yum install git-core",
+    "git clone https://github.com/du6/talkmeup.git",
+    "yum -y install python-pip",
+    "pip install virtualenv",
+    "virtualenv talkmeup-env",
+    "source talkmeup-env/bin/activate",
+    "cd talkmeup",
+    "python manage.py runserver",
+]))
+
+t.add_resource(ec2.Instance(
+    "instance",
+    ImageId="ami-831c2de3",
+    InstanceType="t2.micro",
+    SecurityGroups=[Ref("SecurityGroup")],
+    UserData=ud,
+))
+t.add_output(Output(
+    "InstancePublicIp",
+    Description="Public IP of our instance",
+    Value=GetAtt("instance", "PublicIp"),
+))
+t.add_output(Output(
+    "WebUrl",
+    Description="Application endpoint",
+    Value=Join("", [
+        "http://", GetAtt("instance", "PublicDnsName"),
+        ":",
+        ApplicationPort,
+    ]),
+))
+
+print (t.to_json())
