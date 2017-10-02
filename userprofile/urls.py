23b7735bde7afe2543693535decabadb876110f2
Yuelin Du <yuelindu@Yuelins-MacBook-Air.local>
2017-10-01 23:04:39 -0700

    add test to userprofile

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -4,8 +4,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from userprofile import views
 
 urlpatterns = [
-    url(r'^userprofile/$', views.UserProfileList.as_view()),
-    url(r'^userprofile/(?P<pk>[0-9]+)/$', views.UserProfileDetail.as_view()),
+    url(r'^userprofile/$', views.UserProfileList.as_view(), name='profile-list'),
+    url(r'^userprofile/(?P<pk>[0-9]+)/$', views.UserProfileDetail.as_view(), name='profile-detail'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
