2e509dc767054796564a3fc7db85c8d069598ed8
xiaosaob <mervinmqzju@gmail.com>
2017-10-25 22:01:34 -0700

    Add professors profile.

--- coach/models.py
+++ coach/models.py
@@ -3,17 +3,16 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-def user_directory_path(instance, filename):
-    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
-    return 'user_{0}/{1}'.format(instance.id, filename)
-
 # Create your models here.
 class CoachProfile(models.Model):
     name = models.CharField(max_length=100)
-    avatar = models.ImageField(upload_to=user_directory_path)
+    avatar = models.ImageField(upload_to='coach_photos/%Y/%m/%d')
     created_on = models.DateTimeField(auto_now_add=True)
-    bio_text = models.TextField(max_length=3000)
-    research_area = models.CharField(max_length=500)
+    title = models.CharField(max_length=100, default='')
+    affiliation = models.CharField(max_length=100, default='')
+    secondary_affiliation = models.CharField(max_length=100, default='')
+    bio_text = models.TextField(max_length=3000, default='')
+    research_area = models.CharField(max_length=500, default='')
 
     def __str__(self):
         return self.name
--- talkmeup/urls.py
+++ talkmeup/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^', include('userprofile.urls')),
     url(r'^coach/', include('coach.urls')),
 ]
+
+if settings.DEBUG is True:
+  urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
